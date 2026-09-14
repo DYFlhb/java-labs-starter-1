39e84f551d43a1d2bd7ffc0a3ddf902e7d64c7c3
DYFlhb <113847683+DYFlhb@users.noreply.github.com>
2026-09-15 00:25:44 +0300

    [READY] ЛР 01 — Яричевский Даниил
    Added:
    - isPrime, isPalindrome, average, min, max
    - Tests for every command
    - ParameterizedTest for Prime
    - CLI with prime, palindrome, average

---
+++ lab01/src/main/java/edu/course/lab01/CLItest.java
@@ -0,0 +1,21 @@
+package edu.course.lab01;
+public class CLItest {
+    public static void main(String[] args) {
+        if (args[0].equals("prime")){
+            int number = Integer.parseInt(args[1]);
+            System.out.println(CourseToolkit.isPrime(number));
+        }
+        if (args[0].equals("palindrome")){
+            String text = args[1];
+            System.out.println(CourseToolkit.isPalindrome(text));
+        }
+        if (args[0].equals("average")){
+            int[] values = new int[args.length - 1];
+            for (int i = 1; i < args.length; i++) 
+            {
+                values[i - 1] = Integer.parseInt(args[i]);
+            }
+            System.out.println(CourseToolkit.average(values));
+        }
+    }
+}
--- lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
+++ lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
@@ -1,9 +1,11 @@
 package edu.course.lab01;
 
-import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 class CourseToolkitTest {
 
@@ -19,5 +21,94 @@ class CourseToolkitTest {
         boolean result = CourseToolkit.isEven(7);
 
         assertFalse(result);
+    }
+
+    @Test
+    void returnsTrueForNegativeEvenNumber() {
+        boolean result = CourseToolkit.isEven(-8);
+
+        assertTrue(result);
+    }
+    
+    @Test
+    void returnsTrueIfSevenIsPrime() {
+        boolean result = CourseToolkit.isPrime(7);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIfThirteenIsPrime() {
+        boolean result = CourseToolkit.isPrime(13);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIfFiveIsPrime() {
+        boolean result = CourseToolkit.isPrime(5);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIfElevenIsPrime() {
+        boolean result = CourseToolkit.isPrime(11);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIfStatsIsPalindrome() {
+        boolean result = CourseToolkit.isPalindrome("stats");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIf1029201IsPalindrome() {
+        boolean result = CourseToolkit.isPalindrome("1029201");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueIfCaBaCIsPalindrome() {
+        boolean result = CourseToolkit.isPalindrome("Ca B aC");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsAverage() {
+        double result = CourseToolkit.average(new int[]{1, 2, 3, 4, 5});
+
+        assertEquals(3.0, result);
+    }
+
+    @Test
+    void returnsAverageNegate() {
+        double result = CourseToolkit.average(new int[]{-2, 4, 6});
+
+        assertEquals(8.0/3.0, result);
+    }
+
+    @Test
+    void returnsAverageErrornes() {
+        assertThrows(IllegalArgumentException.class,() -> CourseToolkit.average(null));
+    }
+
+    @Test
+    void returnsMin() {
+        double result = CourseToolkit.min(new int[]{1, 2, 3, 4, 5});
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void returnsMax() {
+        double result = CourseToolkit.max(new int[]{1, 2, 3, 4, 5});
+
+        assertEquals(5, result);
     }
 }
